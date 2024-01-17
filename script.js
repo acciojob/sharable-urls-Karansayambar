ba7abe4f1abe71d9a1db550a17734e79f5cc07d5
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-17 18:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// your code here
+document.addEventListener("DOMContentLoaded", function() {
+  const h3 = document.getElementById("url");
+  const input1 = document.getElementById("name");
+  const input2 = document.getElementById("year");
+  const btn = document.getElementById("button");
+
+  btn.addEventListener("click", function() {
+    const queryString = `?name=${encodeURIComponent(input1.value)}&year=${encodeURIComponent(input2.value)}`;
+    h3.innerText = `https://localhost:8080/${queryString}`;
+  });
+});
+
